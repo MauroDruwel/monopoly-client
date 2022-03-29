b83ef3c80a5031c004236a4d700a5edfe074a665
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-03-29 21:12:03 +0200

    solved join game table

--- src/assets/js/display.js
+++ src/assets/js/display.js
@@ -4,14 +4,17 @@ function showGames(games){
     const $template = document.querySelector('#radio-list').content.firstElementChild.cloneNode(true);
     games.forEach(game => {
         if (game.started === false){
-            $template.querySelector('td').setAttribute("for",game.id);
+            // game name
+            $template.querySelector('td:nth-child(1)').setAttribute("for",game.id);
+            $template.querySelector('td:nth-child(1)').innerHTML=refactorGameID(game.id);
+            // players
             $template.querySelector('td:nth-child(2)').setAttribute("for",game.id);
-            $template.querySelector('td').innerHTML=refactorGameID(game.id);
             $template.querySelector('td:nth-child(2)').innerHTML=game.numberOfPlayers;
+            // radio
             $template.querySelector('td:last-of-type input').setAttribute('id',game.id);
             $template.querySelector('td:last-of-type input').setAttribute('name','Games');
             $template.querySelector('td:last-of-type input').setAttribute('value',game.id);
-            document.querySelector('tbody').insertAdjacentHTML("afterend",$template.outerHTML);
+            document.querySelector('tbody').insertAdjacentHTML("beforeend",$template.outerHTML);
         }
     });
 }
