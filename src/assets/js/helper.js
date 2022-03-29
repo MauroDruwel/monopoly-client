615bc2a6625bf210217d5dd9b2f653c2ef55fd57
Michiel Stragier <Michiel.stragier@student.howest.be>
2022-03-29 22:08:52 +0200

    onclick in js

--- src/assets/js/helper.js
+++ src/assets/js/helper.js
@@ -3,6 +3,9 @@
 function fetchGames(myfunction) {
     fetchFromServer('/games', 'GET').then(games => myfunction(games)).catch(errorHandler);
 }
+function goToJoinCreate(){
+    location.href = "join-create.html";
+}
 function refactorGameID(gameID){
     let array_of_characters_of_game_id = gameID.split("");
     for (let i = 0; i < 4 ; i++) {
--- src/assets/js/initialiser.js
+++ src/assets/js/initialiser.js
@@ -7,11 +7,16 @@ function init(){
     if (document.querySelector('#join-create')){
         initJoinCreate();
     }
+    else if (document.querySelector('#index')){
+        initIndex();
+    }
 }
 
 function initJoinCreate(){
     fetchGames(showGames);
     document.querySelector("#join-game button[type=submit]").addEventListener("click",joinAGame);
     document.querySelector("#create-game button[type=submit]").addEventListener('click',createGame);
-
+}
+function initIndex(){
+    document.querySelector("#start-game").addEventListener('click',goToJoinCreate);
 }
