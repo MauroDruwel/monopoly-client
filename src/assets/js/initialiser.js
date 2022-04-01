09333e1349e55e3f9175dfbad02cf829fe83b5c4
Lucas Guillemyn <lucas.guillemyn@student.howest.be>
2022-04-01 15:07:15 +0200

    removed test function from initialiser.js

--- src/assets/js/initialiser.js
+++ src/assets/js/initialiser.js
@@ -8,7 +8,6 @@ let _player = {
 document.addEventListener('DOMContentLoaded',init);
 
 function init(){
-    processProperty("Baltic")
     if (document.querySelector('#index')){
         initIndex();
     }
