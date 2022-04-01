b576fc871effdb135f7a2fbf09f866cee4261fb7
luca verlinde <luca.verlinde@student.howest.be>
2022-04-01 16:20:39 +0200

    added js file

---
+++ src/assets/js/winner-screen/winner-screen.js
@@ -0,0 +1,9 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
+
+function partyMode() {
+    document.getElementById("=party_background").style.backgroundImage = "url(https://cdn.discordapp.com/attachments/955800521409703966/959439396560584704/fortnite-monopoly.gif)";
+}
