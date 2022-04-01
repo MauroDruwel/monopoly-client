70527354255492e97b8f1be00fce71ac405012bc
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-04-01 02:04:24 +0200

    added add event listener helper for multiple elements

--- src/assets/js/helper.js
+++ src/assets/js/helper.js
@@ -1,8 +1,11 @@
 "use strict";
+
+/* Server Fetch Helpers */
 function gameState(myfunction){
     fetchFromServer(`/games/${_player.gameId}`,'GET').then(response => myfunction(response)).catch(errorHandler);
 }
 
+/* String Convert Helpers */
 function beautifyId(id){
     return capitalizeFirstLetter(id.replace(/_/g, ' '));
 }
@@ -13,4 +16,11 @@ function capitalizeFirstLetter(string) {
 
 function convertSpacesToUnderscores(string){
     return string.replace(/ /g, "_");
+}
+
+/* Event Helpers */
+
+function addEventListenerToElements(type, handler, selector){
+    const $elements = document.querySelectorAll(selector);
+    $elements.forEach(($element) => $element.addEventListener(type, handler));
 }
