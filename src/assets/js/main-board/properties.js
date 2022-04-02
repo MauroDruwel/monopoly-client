a7dbcf66e005e20d810831d2f428a1454627cbf6
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-04-02 19:08:13 +0200

    updated js according to new property templates + refactored

--- src/assets/js/main-board/properties.js
+++ src/assets/js/main-board/properties.js
@@ -16,3 +16,23 @@ function processProperty(property){
     processPropertyFront(property);
     processPropertyBack(property);
 }
+
+function retrievePropertyState(game, property) {
+    // default property values ["owner: none", "houses: 0", "hotels: 0", "mortgaged? FALSE"]
+    const propertyState = ["none", 0, 0, "FALSE"];
+
+    game.players.forEach(player => {
+        player.properties.forEach(ownedProperty => {
+            if (ownedProperty.property === property) {
+                propertyState[0] = player.name;
+            }
+            propertyState[1] = ownedProperty.houseCount;
+            propertyState[2] = ownedProperty.hotelCount;
+
+            if (ownedProperty.mortgage) {
+                propertyState[3] = "TRUE";
+            }
+        });
+    });
+    return propertyState;
+}
--- src/assets/js/render/render-property.js
+++ src/assets/js/render/render-property.js
@@ -1,51 +1,48 @@
 "use strict";
 
 function renderPropertyBack(property) {
-    const $tbody = document.querySelector('.property-card-back');
-    $tbody.querySelector('h3').innerHTML = property.name;
-    $tbody.querySelector('h4 span').innerHTML = property.rent;
 
-    $tbody.querySelector('ul').insertAdjacentHTML("beforeend", `<li>With 1 house: ${property.rentWithOneHouse}</li>
-                                                                                   <li>With 2 houses: ${property.rentWithTwoHouses}</li>
-                                                                                   <li>With 3 houses: ${property.rentWithThreeHouses}</li>
-                                                                                   <li>With 4 houses: ${property.rentWithFourHouses}</li>
-                                                                                   <li>With hotel: ${property.rentWithHotel}</li>`);
+    // data set in selector is added. Why? Select specific property from html to render with this data-tile value !!!
+    // What does this mean? The data-tile value needs to be set before this function is called! [! IMPORTANT]
+    const $properties = document.querySelectorAll(`[data-tile="${property.nameAsPathParameter}"].property-back`);
+
+    $properties.forEach($property => {
+        $property.querySelector('.grid-header h3').innerHTML = property.name;
+        $property.querySelector('.grid-body h4:first-of-type span').innerHTML = property.rent;
+
+        $property.querySelector('.grid-body ul').insertAdjacentHTML("beforeend",
+            `<li>With 1 house: ${property.rentWithOneHouse}</li>
+                <li>With 2 houses: ${property.rentWithTwoHouses}</li>
+                <li>With 3 houses: ${property.rentWithThreeHouses}</li>
+                <li>With 4 houses: ${property.rentWithFourHouses}</li>
+                <li>With hotel: ${property.rentWithHotel}</li>`
+        );
 
 
-    $tbody.querySelector('ul + h4 span').innerHTML = property.mortgage;
-    $tbody.querySelector('p:first-of-type span').innerHTML = property.housePrice;
-    $tbody.querySelector('p + p span').innerHTML = property.housePrice;
-
-}
-
-function renderPropertyFront(searchProperty, game) {
-    const $tbody = document.querySelector('.property-card-front');
-    const propertyState = ["none", 0, 0];
-    game.players.forEach(player => {
-        player.properties.forEach(property => {
-            if (property.property === searchProperty) {
-                propertyState[0] = player.name;
-            }
-            propertyState[1] = property.houseCount;
-            propertyState[2] = property.hotelCount;
-
-            if (property.mortgage) {
-                $tbody.querySelector('p:last-of-type span').innerHTML = "yes";
-            }
-            else{
-                $tbody.querySelector('p:last-of-type span').innerHTML = "no";
-
-            }
-        });
+        $property.querySelector('.grid-body h4:last-of-type span').innerHTML = property.mortgage;
+        $property.querySelector('.grid-body p:first-of-type span').innerHTML = property.housePrice;
+        $property.querySelector('.grid-body p:first-of-type + p span').innerHTML = property.housePrice;
     });
-
-
-    $tbody.querySelector('h3').innerHTML = searchProperty;
-    $tbody.querySelector('p:first-of-type').innerHTML = propertyState[0];
-
-    $tbody.querySelector('ul').insertAdjacentHTML("beforeend", `<li>${propertyState[1]} house(s)</li>
-                                                                                   <li>${propertyState[2]} hotel</li>`);
-
 }
 
 
+
+function renderPropertyFront(property, game) {
+
+    // data set in selector is added. Why? Select specific property from html to render with this data-tile value !!!
+    // What does this mean? The data-tile value needs to be set before this function is called! [! IMPORTANT]
+    const $properties = document.querySelectorAll(`[data-tile="${property.nameAsPathParameter}"].property-front`);
+
+    $properties.forEach($property => {
+
+        const propertyState = retrievePropertyState(game, property);
+
+        $property.querySelector('.grid-header h3').innerHTML = property;
+        $property.querySelector('.grid-body p:first-of-type').innerHTML = propertyState[0]; // set owner
+        $property.querySelector('.grid-body ul').insertAdjacentHTML("beforeend",
+            `<li>${propertyState[1]} house(s)</li>
+                <li>${propertyState[2]} hotel</li>`
+        );
+        $property.querySelector('.grid-body p:last-of-type span').innerHTML = propertyState[3];
+    });
+}
