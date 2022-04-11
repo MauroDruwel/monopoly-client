8e5ecc2bd4481cab28b7e64576702b18d29f3d35
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-04-11 23:18:57 +0200

    moved code from render-tiles.js to render-carousel.js

--- src/assets/js/render/render-carousel.js
+++ src/assets/js/render/render-carousel.js
@@ -15,17 +15,69 @@ function renderCarousel(){
     }
 }
 
+function renderTile(tile, scale="") {
+    switch(tile.type) {
+        case "Go":
+            renderGoTile(scale);
+            break;
+        case "street":
+            renderStreetTile(scale);
+            break;
+        case "community chest":
+            renderCommunityChestTile(scale);
+            break;
+        case "Tax Income":
+            renderTaxIncomeTile(scale);
+            break;
+        case "railroad":
+            renderRailroadTile(scale);
+            break;
+        case "chance":
+            renderChanceTile(scale);
+            break;
+        case "Jail":
+            renderJailTile(scale);
+            break;
+        case "utility":
+            renderUtilityTile(scale);
+            break;
+        case "Free Parking":
+            renderFreeParkingTile(scale);
+            break;
+        case "Go to Jail":
+            renderGoToJailTile(scale);
+            break;
+        case "Luxury Tax":
+            renderLuxuryTaxTile(scale);
+            break;
+        default:
+            throw "Couldn't render tile";
+    }
+}
 
+function renderGoTile(scale=""){
+    renderBasicTile(".go-template", scale);
+}
 
-/* const movies = json.Search;
+function renderCommunityChestTile(scale=""){
+    renderBasicTile(".community-chest-template", scale);
+}
 
-movies.forEach(movie => {
-    const $template = document.querySelector("#movies template").content.firstElementChild.cloneNode(true);
+function renderChanceTile(scale=""){
+    renderBasicTile(".chance-template", scale);
+}
 
-    $template.dataset.imdbid = movie.imdbID;
-    $template.querySelector('img').setAttribute('src', movie.Poster);
-    $template.querySelector('img').setAttribute('alt', movie.Title);
-    $template.querySelector('img').setAttribute('title', movie.Title);
-    $template.querySelector('figcaption').innerText = movie.Title;
-    document.querySelector("#movies").insertAdjacentHTML('beforeend', $template.outerHTML);
-}); */
+function renderJailTile(scale=""){
+    renderBasicTile("");
+}
+
+function renderTaxIncomeTile(scale=""){
+    // TODO: change to actual tile
+    renderBasicTile(".community-chest-template", scale);
+}
+
+function renderBasicTile(template, scale=""){
+    const $template = document.querySelector(`#carousel ${template}`).content.firstElementChild.cloneNode(true);
+    $template.classList.add(scale);
+    document.querySelector('#carousel').insertAdjacentHTML('beforeend', $template.outerHTML);
+}
