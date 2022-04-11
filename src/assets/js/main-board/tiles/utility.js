6fc5be40a3fa576b7b135a538fa12ea08667af47
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-04-12 01:19:55 +0200

    added mortgage value to utility template

--- src/assets/js/main-board/tiles/utility.js
+++ src/assets/js/main-board/tiles/utility.js
@@ -2,12 +2,19 @@
 
 function processUtilityTile(name){
     const owner = retrieveTileOwner(name);
+
     let ownerName = "none";
+    let mortgage = "false";
     if(owner){
         ownerName = owner.name;
+        for(const ownedProperty of owner.properties){
+            if(ownedProperty.property === convertUnderscoresToSpaces(name)){
+                mortgage = ownedProperty.mortgage;
+            }
+        }
     }
     const utility = _tiles[tilePosition(name)];
-    renderUtilityTile(utility, ownerName);
+    renderUtilityTile(utility, ownerName, mortgage);
 }
 
 
