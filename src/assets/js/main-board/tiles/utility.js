b332acc586215a230393a257ecb311082fe81699
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-04-12 00:10:31 +0200

    process and render utility tile (untested)

--- src/assets/js/main-board/tiles/utility.js
+++ src/assets/js/main-board/tiles/utility.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function processUtilityTile(name){
+    const owner = retrieveUtilityOwner(name);
+    renderUtilityTile(name, owner);
+}
+
+function retrieveUtilityOwner(name){
+    for(const player of _players){
+        for (const ownedProperty of player.properties){
+            if(ownedProperty.property === convertUnderscoresToSpaces(name)){
+                return player.name;
+            }
+        }
+    }
+    return "None";
+}
--- src/assets/js/render/tiles/render-utility.js
+++ src/assets/js/render/tiles/render-utility.js
@@ -1,2 +1,17 @@
 "use strict";
 
+function renderUtilityTile(name, owner){
+
+    // data set in selector is added. Why? Select specific tile from html to render with this data-tile value !!!
+    // What does this mean? The data-tile value needs to be set before this function is called! [! IMPORTANT]
+    const utility = _tiles[tilePosition(name)];
+    const $utilities = document.querySelectorAll(`[data-tile="${utility.nameAsPathParameter}"]`);
+
+    $utilities.forEach($utility => {
+        $utility.dataset.position = utility.position;
+
+        $utility.querySelector('.header-grid h3').innerHTML = utility.name;
+        $utility.querySelector('.footer-grid h4:first-of-type').innerHTML = utility.cost;
+        $utility.querySelector('.footer-grid h4:last-of-type').innerHTML = owner;
+    });
+}
