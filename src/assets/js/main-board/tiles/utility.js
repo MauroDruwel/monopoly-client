beb74e2dbb64ad2d47c14b891f8d3d52c3c964c3
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-04-12 00:29:34 +0200

    retrieveOwner moved to helper.js

--- src/assets/js/main-board/tiles/utility.js
+++ src/assets/js/main-board/tiles/utility.js
@@ -1,9 +1,13 @@
 "use strict";
 
 function processUtilityTile(name){
-    const owner = retrieveTileOwner(name).name;
+    const owner = retrieveTileOwner(name);
+    let ownerName = "None";
+    if(owner){
+        ownerName = owner.name;
+    }
     const utility = _tiles[tilePosition(name)];
-    renderUtilityTile(utility, owner);
+    renderUtilityTile(utility, ownerName);
 }
 
 
