dd96f4d890e4877f0b2fead3b709343ec7aa5cd7
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-04-12 02:48:06 +0200

    refactored code with global variable instead of fetchFromServer

--- src/assets/js/main-board/tile-map.js
+++ src/assets/js/main-board/tile-map.js
@@ -1,14 +1,11 @@
 "use strict";
 
 function processTileMap(username){
-    fetchFromServer(`/games/${_player.gameId}`, 'GET').then(gameState => {
-        const players = gameState.players;
-        players.forEach(player => {
-            if(player.name === username){
-                renderTileMap(player);
-            }
-        });
-    });
+    for(const player of _players){
+        if(player.name === username){
+            renderTileMap(player);
+        }
+    }
 }
 
 function processTileMapNavigation(e){
