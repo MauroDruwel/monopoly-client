55ba3adba1545216f08d868c07404327e086e5d3
Lucas Guillemyn <lucas.guillemyn@student.howest.be>
2022-04-14 00:22:10 +0200

    js done not sure if it works

--- src/assets/js/main-board/auction.js
+++ src/assets/js/main-board/auction.js
@@ -10,3 +10,39 @@ function bid(e){
     };
     fetchFromServer(`/games/${_player.gameId}/players/${_player.username}/auctions/${propertyName}/bid`, 'POST', requestBody).catch(errorHandler);
 }
+function checkToSellBuildings(){
+    const $checkbox = document.querySelector("#setup-auction button[value=sell-buildings]");
+    const tiles = [];
+    if ($checkbox.classList.contains("active")){
+        if (_tiles[_player.carousel].type === "steet"){
+            const streetColor = _tiles[_player.carousel].color;
+            _tiles.forEach(tile =>{
+                if (tile === streetColor){
+                    tiles.push(tile.name);
+                }
+            });
+        }
+        sellBuildings(tiles);
+    }
+}
+
+function sellBuildings(tiles){
+    let highestHouseCount = 0;
+    _game.players.forEach(player => {
+        do {
+            tiles.forEach(tile => {
+                player.properties.forEach(propertie => {
+                    if (propertie.houseCount > highestHouseCount) {
+                        highestHouseCount = propertie.houseCount;
+                    }
+                    if (tile === propertie.name && propertie.hotelCount !== 0) {
+                        notBuyProperty("hotel");
+                    } else if (tile === propertie.name && propertie.houseCount !== 0) {
+                        notBuyProperty("houses");
+                    }
+                });
+            });
+            highestHouseCount--;
+        }while (highestHouseCount > 0);
+    });
+}
