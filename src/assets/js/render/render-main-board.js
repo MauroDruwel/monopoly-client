e1a57417f746b75ed2c48ebe271ace353df8281e
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-04-16 02:29:32 +0200

    render buy house

--- src/assets/js/render/render-main-board.js
+++ src/assets/js/render/render-main-board.js
@@ -6,3 +6,13 @@ function renderDiceRoll(){
         document.querySelector('#dice-container .dice-left').innerHTML = _game["lastDiceRoll"][1];
     }
 }
+
+function renderBuyHouse(tile){
+    addClassToElements('#main-board > section', 'hidden');
+    document.querySelector('#buy-house').classList.remove('hidden');
+    document.querySelectorAll('#buy-house .tile').forEach($element => {
+        $element.dataset.tile = tile.nameAsPathParameter;
+    });
+    processProperty(tile.name);
+    document.querySelector('#buy-house .information h2 span').innerHTML = tile.housePrice;
+}
