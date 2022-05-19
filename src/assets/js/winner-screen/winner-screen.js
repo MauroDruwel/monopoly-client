0c5208c8bd5f6a5dd1735240cd733743a0bbe9b1
luca verlinde <luca.verlinde@student.howest.be>
2022-05-19 17:23:12 +0200

    changed font of activity a little bit bolder, because of reviews from people

--- src/assets/js/winner-screen/winner-screen.js
+++ src/assets/js/winner-screen/winner-screen.js
@@ -6,4 +6,3 @@ function changeBackground() {
     $h2.innerHTML = "";
     $body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/955800521409703966/959439396560584704/fortnite-monopoly.gif')";
 }
-
