8709bddfa7645b29116463d803df943d7361612b
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-05-20 17:36:20 +0200

    select only h2 tag

--- src/assets/js/winner-screen/winner-screen.js
+++ src/assets/js/winner-screen/winner-screen.js
@@ -2,7 +2,7 @@
 
 function changeBackground() {
     const $body = document.querySelector("#winner-screen main");
-    const $h2 = $body.querySelectorAll("h2, p");
+    const $h2 = $body.querySelector("h2");
     $h2.innerHTML = "";
     $body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/955800521409703966/959439396560584704/fortnite-monopoly.gif')";
 }
